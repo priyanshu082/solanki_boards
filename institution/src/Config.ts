49f323a1edb7c127d78067f33804ab5908321bdd
priyanshu082 <priyanshusingh216@gmail.com>
2025-02-26 18:20:23 +0530

    INSTITUTION :: some chnges

--- institution/src/Config.ts
+++ institution/src/Config.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = import.meta.env.VITE_API_URL;
-export const BASE_URL = '/api';
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const ADMIN_BASE_URL = BASE_URL + '/admin';
 export const INSTITUTE_BASE_URL = BASE_URL + '/institute';
